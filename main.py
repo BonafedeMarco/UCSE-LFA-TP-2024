21739e0290a467d05b285791d1bee6998e14d1f1
Marco Bonafede <marcobonafede@hotmail.com>
2024-10-13 01:49:02 -0300

    agregada gramática de los slides de clase para probar cosas

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from gramatica import Gramatica
 
 prueba = Gramatica()
-prueba.setear("S:a Q\nQ:b R\nQ:c R\nR:d\nU:U b")
+#prueba.setear("S:a Q\nQ:b R\nQ:c R\nR:d\nU:U b")
+prueba.setear("S:X Y Z\nX:a\nX:b\nX:lambda\nY:a\nY:d\nY:lambda\nZ:e\nZ:f\nZ:lambda")
 print(prueba)
 
 
