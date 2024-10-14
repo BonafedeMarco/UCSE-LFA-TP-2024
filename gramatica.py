cd967a9f12ac84438249a3913d2f0a59a6ad423e
Marco Bonafede <marcobonafede@hotmail.com>
2024-10-13 23:45:11 -0300

    ahora si fixeados los first (allegedly)

--- gramatica.py
+++ gramatica.py
@@ -148,10 +148,10 @@ class Gramatica:
         firsts = []
         for produccion in self.reglas[nt]["producciones"]:
             if len(self.reglas[nt]["producciones"][produccion]["first"]) == 0:
-                firsts = []
                 simbolos = produccion.split()
                 for simbolo in simbolos:
                     if simbolo[0].isupper():
+                        firsts = []
                         firsts.extend(self.obtener_firsts(simbolo))
                         self.reglas[nt]["producciones"][produccion]["first"] = list(set(firsts))
                     else:
