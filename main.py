e0901d4241f18fed296139b8013b4351cec0acc5
Marco Bonafede <marcobonafede@hotmail.com>
2024-10-14 02:12:32 -0300

    reacomodamiento para probar más gramáticas más fácil

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 from gramatica import Gramatica
 
 prueba = Gramatica()
-prueba.setear("S:a Q\nQ:b R\nQ:c R\nR:d\nU:U b")
-print(prueba)
-print("-----------------------------------------------------------------------------")
-prueba.setear("S:X Y Z\nX:a\nX:b\nX:lambda\nY:a\nY:d\nY:lambda\nZ:e\nZ:f\nZ:lambda")
-print(prueba)
-print("-----------------------------------------------------------------------------")
-prueba.setear("S:a Q\nS:a R\nQ:b\nR:b\nP:d")
-print(prueba)
-print("-----------------------------------------------------------------------------")
-prueba.setear("S:a S\nS:Q\nQ:b")
-print(prueba)
-print("-----------------------------------------------------------------------------")
-prueba.setear("S:P\nS:Q\nQ:b Q\nP:a P\nP:lambda")
-print(prueba)
-print("-----------------------------------------------------------------------------")
 
+gramaticas = []
+
+gramaticas.append("S:a Q\nQ:b R\nQ:c R\nR:d\nU:U b")
+gramaticas.append("S:X Y Z\nX:a\nX:b\nX:lambda\nY:a\nY:d\nY:lambda\nZ:e\nZ:f\nZ:lambda")
+gramaticas.append("S:a Q\nS:a R\nQ:b\nR:b\nP:d")
+gramaticas.append("S:a S\nS:Q\nQ:b")
+gramaticas.append("S:P\nS:Q\nQ:b Q\nP:a P\nP:lambda")
+
+for reglas in gramaticas:
+    prueba.setear(reglas)
+    print(prueba)
+    print("-----------------------------------------------------------------------------")
+    print()
