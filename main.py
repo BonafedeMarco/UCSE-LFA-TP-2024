bd4f8dd5e1981744575cedef0ed75a06760a0f96
Marco Bonafede <marcobonafede@hotmail.com>
2024-10-13 23:17:14 -0300

    cambiado para poder probar varias gramaticas a la vez fuera de tests

--- main.py
+++ main.py
@@ -2,8 +2,16 @@ from gramatica import Gramatica
 
 prueba = Gramatica()
 prueba.setear("S:a Q\nQ:b R\nQ:c R\nR:d\nU:U b")
-#prueba.setear("S:X Y Z\nX:a\nX:b\nX:lambda\nY:a\nY:d\nY:lambda\nZ:e\nZ:f\nZ:lambda")
-#prueba.setear("S:a Q\nS:a R\nQ:b\nR:b\nP:d")
 print(prueba)
+print("-----------------------------------------------------------------------------")
+prueba.setear("S:X Y Z\nX:a\nX:b\nX:lambda\nY:a\nY:d\nY:lambda\nZ:e\nZ:f\nZ:lambda")
+print(prueba)
+print("-----------------------------------------------------------------------------")
+prueba.setear("S:a Q\nS:a R\nQ:b\nR:b\nP:d")
+print(prueba)
+print("-----------------------------------------------------------------------------")
+prueba.setear("S:a S\nS:Q\nQ:b")
+print(prueba)
+print("-----------------------------------------------------------------------------")
 
 
